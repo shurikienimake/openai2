2273fb53f78f7857881519079154e80fe278c00f
shurikienimake <114287414+shurikienimake@users.noreply.github.com>
2023-02-22 12:49:55 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'f8e47c8b5263bd917afff119b4a219c3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/f8e47c8b5263bd917afff119b4a219c3/invoke.js"></scr' + 'ipt>');
