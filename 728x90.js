5d1fc20cb74b0e9721dc6278a88a92543581b907
shurikienimake <114287414+shurikienimake@users.noreply.github.com>
2023-07-30 05:53:10 +0700

    Update 728x90.js

--- 728x90.js
+++ 728x90.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : 'f8e47c8b5263bd917afff119b4a219c3',
+		'key' : 'c853da16437a99d8a68c536b29989664',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/f8e47c8b5263bd917afff119b4a219c3/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c853da16437a99d8a68c536b29989664/invoke.js"></scr' + 'ipt>');
